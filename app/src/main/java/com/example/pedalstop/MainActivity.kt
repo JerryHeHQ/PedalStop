38d0e6439c6d443c691eadded351c6f17b093191
JerryHeHQ <github@exohorizon.tech>
2024-04-05 03:02:57 -0500

    Implemented Bottom Navigation Bar

--- app/src/main/java/com/example/pedalstop/MainActivity.kt
+++ app/src/main/java/com/example/pedalstop/MainActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.ViewConfiguration
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.pedalstop.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var authUser : AuthUser
     private val mainViewModel : MainViewModel by viewModels()
 
@@ -37,10 +47,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // enableEdgeToEdge()
-        initMenu()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initMenu()
+
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_search,
+                R.id.navigation_saved,
+                R.id.navigation_posts,
+                R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onStart() {
@@ -51,5 +73,10 @@ class MainActivity : AppCompatActivity() {
             mainViewModel.setCurrentAuthUser(it)
             binding.username.text = it.name
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
